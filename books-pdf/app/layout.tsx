0c238ce02219aff3ee2d5b0339b9c8487db7fbee
Kamleshmohabe1 <kamleshmohabe1@gmail.com>
2025-12-01 21:15:16 +0530

    third commit

--- books-pdf/app/layout.tsx
+++ books-pdf/app/layout.tsx
@@ -1,28 +1,25 @@
-import { Inter, Poppins } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
 import { AuthProvider } from '@/context/AuthContext';
 import { Toaster } from 'react-hot-toast';
-import { ReactNode } from 'react';
 const inter = Inter({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"], // only include weights you need
   display: "swap",
   preload: true
 });
-const poppins = Poppins({ 
-    weight: ['400', '500', '600', '700', '800'],
-    subsets: ['latin'],
-    variable: '--font-poppins',
-    preload: true,
-});
 
 export const metadata = {
-    title: 'BookMarket - Free & Premium PDF Books & eBooks Plateform',
+    title: 'BooksPDF - Free & Premium PDF Books & eBooks Plateform',
     description: 'Download free & thousands of eBooks across programming, business, design, and more. Secure payment via PhonePe.',
-    keywords: 'pdf books, ebooks, online books, digital books, programming books, business books',
+    keywords: 'pdf books, ebooks, online books, digital books, programming books, business books, books pdf',
 };
 
-export default function RootLayout({ children }: { children:  React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
     return (
         <html lang="en">
             <body className={inter.className}>
