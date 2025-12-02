63c9c54015b32aa21a809aabef333587fefd8fc5
Kamleshmohabe1 <kamleshmohabe1@gmail.com>
2025-12-02 15:30:18 +0530

    sixth1

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
-const { verifyPhonePeConfig } = require('./config/phonepe');
+// const { verifyPhonePeConfig } = require('./config/phonepe');
 
 // Load env vars
 dotenv.config();
