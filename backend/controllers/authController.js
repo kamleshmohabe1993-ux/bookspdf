a2537025cdb7e577721665aa9891a19abfabad9d
Kamleshmohabe1 <kamleshmohabe1@gmail.com>
2025-12-07 18:30:30 +0530

    forget password1

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const { sendOTPEmail } = require('../services/emailService');
 const otpService = require('../services/otpServices');
